8a0c9157dc7ee9cd521db630007b1e960697931e
coachstevengarcia <coachstevengarcia@gmail.com>
2022-11-07 18:46:06 -0600

    Fixed end of code

--- swordfish.py
+++ swordfish.py
@@ -5,6 +5,6 @@ while True:
         continue
     print('Hello, Steven. What is the password? (It is a fish.)')
     password = input()
-        if password == 'swordfish':
-            break
-    print('Access Granted.')
+    if password == 'swordfish':
+        break
+print('Access Granted.')
